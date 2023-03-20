757f3d291fd1dcce1bf7245c44a2ed0294f3eb64
lws2269 <lws2269@kakao.com>
2023-03-20 21:25:37 +0900

    feat: 프로젝트 생성, 스토리보드 삭제 및 코드베이스 UI 작성을 위한 SceneDelegate 수정

---
+++ OTT_Clone-CodeBase/OTT_Clone-CodeBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OTT_Clone-CodeBase
+//
+//  Created by leewonseok on 2023/03/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
